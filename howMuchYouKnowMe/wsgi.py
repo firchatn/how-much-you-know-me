32d5bddcb81fb997682cbcff8f9fc26cc707071d
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-23 11:27:18 +0100

    first step init project

---
+++ howMuchYouKnowMe/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for howMuchYouKnowMe project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "howMuchYouKnowMe.settings")
+
+application = get_wsgi_application()
