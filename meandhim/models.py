9f0a8f4e6c6237f6a67c80bb528e9458803b02f8
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-23 22:22:58 +0100

    TOFIX database conception

--- meandhim/models.py
+++ meandhim/models.py
@@ -7,3 +7,27 @@ class user(models.Model):
 	name = models.CharField(max_length=20)
 	def __str__(self):
 		return self.name
+"""
+class response(models.Model):
+	name = models.CharField(max_length=20)
+	def __str__(self):
+		return self.name
+
+
+class answer(models.Model):
+	name = models.CharField(max_length=20)
+	def __str__(self):
+		return self.name
+
+class question(models.Model):
+	id = models.IntegerField()
+	name = models.CharField(max_length=20)
+	def __str__(self):
+		return self.name
+
+class choice(models.Model):
+	id = models.IntegerField()
+	name = models.CharField(max_length=20)
+	def __str__(self):
+		return self.name
+"""
